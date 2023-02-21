ba96dfa6166ca22ec55590854e42c7998de3dd07
Kaan Yeyrek <kaanyeyrek@icloud.com>
2023-02-21 20:53:32 +0300

    fixed bug

---
+++ GamerCaseApp/CoreDataManager/Games+CoreDataClass 2.swift
@@ -0,0 +1,15 @@
+//
+//  Games+CoreDataClass.swift
+//  GamerCaseApp
+//
+//  Created by Kaan Yeyrek on 2/18/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Games: NSManagedObject {
+
+}
--- GamerCaseApp/Network/Base/CoreService/NetworkHelper.swift
+++ GamerCaseApp/Network/Base/CoreService/NetworkHelper.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 enum NetworkHelper {
-    static let apiKey = "504774123bfe43b784eb459456f504ac"
+    static let apiKey = APIManager().loadApiKey()
 }
+final class APIManager {
+    let keychainManager = KeychainManager()
+    let serviceName = "My_App"
+    let accountName = "kaanyeyrek@icloud.com"
+    
+    func saveApiKey(apiKey: String) -> Bool {
+           return keychainManager.saveApiKey(apiKey: apiKey, serviceName: serviceName, accountName: accountName)
+       }
+    func loadApiKey() -> String? {
+           return keychainManager.loadApiKey(serviceName: serviceName, accountName: accountName)
+       }
+}
+
